46e92b0cb3953cf571f8d8f2ba38ea2361610c9e
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-05-02 14:59:37 -0300

    página "admin" criada

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,7 +1,38 @@
-import './admin.css'
+import { useState } from "react";
+import "./admin.css";
 
 export default function Admin() {
-    return(
-        <h1>Pagina Admin</h1>
-    )
+  const [tarefaInput, setTarefaInput] = useState("");
+
+  function handleRegister(e) {
+    e.preventDefault();
+    alert("clicou");
+    console.log(tarefaInput);
+  }
+
+  return (
+    <div className="admin-container">
+      <h1>Minhas Tarefas</h1>
+
+      <form onSubmit={handleRegister}>
+        <textarea
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+          placeholder="Digite sua tarefa..."
+        />
+        <button type="submit">Registar Tarefa</button>
+      </form>
+
+      <article className="list">
+        <p>Estudar JavaScript e ReactJS</p>
+
+        <div>
+            <button>Editar</button>
+            <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout">Sair</button>
+    </div>
+  );
 }
